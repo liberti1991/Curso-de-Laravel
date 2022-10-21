69fbf680746a0df6bde9950295b2f5ae8fe9aabc
Liberti1991 <liberti1991@gmail.com>
2022-10-21 13:11:20 -0300

    criando controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "ola ";
-});
+Route::get('/', 'PrincipalController@principal');
 
 Route::get('/sobre-nos', function () {
     return "ola2 ";
