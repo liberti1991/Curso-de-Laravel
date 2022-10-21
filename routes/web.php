17800da6c544121d16f87e48aa7c2f68ecc2716e
Liberti1991 <liberti1991@gmail.com>
2022-10-21 14:47:24 -0300

    parametro na route

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobreNos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
+
+// Route::get('/contato/{nome}', function (string $nome) {
+//     echo 'Estamos aqui ' . $nome;
+// });
+
+Route::get('/contato/{nome}/{sobrenome}', function (string $nome, string $sobrenome) {
+    echo 'Estamos aqui ' . $nome . ' ' . $sobrenome;
+});
