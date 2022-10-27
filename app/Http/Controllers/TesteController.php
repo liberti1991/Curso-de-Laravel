555336ddbb1b5998deba3c9a36290f45cb945c5e
Liberti1991 <liberti1991@gmail.com>
2022-10-27 12:36:27 -0300

    metodo with

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -9,7 +9,11 @@ class TesteController extends Controller
     public function teste(int $p1, $p2)
     {
         // echo "A soma de $p1 e $p2 é:" . ($p1 + $p2);
+
         // return view('site.teste', ['x' => $p1, 'y' => $p2]); array associativo
-        return view('site.teste', compact("p1", "p2")); //compact
+
+        // return view('site.teste', compact("p1", "p2")); //compact
+
+        return view('site.teste')->with('p1', $p1)->with('p2', $p2);
     }
 }
