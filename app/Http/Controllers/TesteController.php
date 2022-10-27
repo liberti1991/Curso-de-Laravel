25cf67838aee3af6d8a4ef83143b7276436e646e
Liberti1991 <liberti1991@gmail.com>
2022-10-27 12:34:14 -0300

    compact

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -9,6 +9,7 @@ class TesteController extends Controller
     public function teste(int $p1, $p2)
     {
         // echo "A soma de $p1 e $p2 é:" . ($p1 + $p2);
-        return view('site.teste', ['x' => $p1, 'y' => $p2]);
+        // return view('site.teste', ['x' => $p1, 'y' => $p2]); array associativo
+        return view('site.teste', compact("p1", "p2")); //compact
     }
 }
