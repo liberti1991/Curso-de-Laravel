34044ef31a417e2e1bfc4709349a77a3ebf6654f
Liberti1991 <liberti1991@gmail.com>
2022-10-28 18:54:33 -0300

    unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -3,20 +3,26 @@
 {{--fica o comentario--}}
 
 {{-- @dd($fornecedores); --}}
-
+{{--
 @if(count($fornecedores) > 0 && count($fornecedores)< 10)
     <h3>Exite informaçoes no array</h3>
 @elseif(count($fornecedores)>10)
     <h3>varios fornecedores</h3>
 @else
     <h3>nao tem nada aqui arrombado </h3>
-@endif
+@endif --}}
 
 
+{{-- operador @unless === negação --}}
 
+<p>fornecedor: {{ $fornecedores[0]['nome']}}</p>
+<p>Status: {{ $fornecedores[0]['status']}}</p>
 
-{{-- @php
+{{-- @if($fornecedores[0]['status'] === 'N')
+    fornecedor inativo
+@endif --}}
 
+@unless($fornecedores[0]['status'] === 'S')
+    fornecedor falso
+@endunless
 
-
-@endphp --}}
